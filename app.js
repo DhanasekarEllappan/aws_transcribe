fd92484efea34c14ee9cef0b93e4053990577378
Dhanasekar Ellappan <dhanasekar.ellappan@avasoft.com>
2025-07-23 13:43:55 +0530

    session token update

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ const transcribeClient = new TranscribeStreamingClient({
   region: process.env.AWS_REGION,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    sessionToken: process.env.AWS_SESSION_TOKEN
   },
   requestHandler: new NodeHttp2Handler()
 });
